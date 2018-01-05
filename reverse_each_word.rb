b02add73cc00278febdc09d47195eb6dd47ba8f5
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-05 16:18:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  sentence_array.collect {|i| i.reverse}
+  sentence_array.collect do |i|
+    i.reverse
+  end
   sentence_array.join(" ")
 end
